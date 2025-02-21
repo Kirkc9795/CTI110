1a1feb1e7bee441f202e970bb6881b50a7a2802d
Kirkc9795 <kirkc9795@student.faytechcc.edu>
2025-02-21 11:00:01 -0500

    Add files via upload

---
+++ P1Lab1_KirkChristopher.py
@@ -0,0 +1,7 @@
+#Christopher kirk
+#02/20/2025
+#P1LAB1
+# This project is me getting an input of a name and and welcoming them into this course
+first_name = input("Enter your first name:")
+last_name = input ("Enter your last name:")
+print("Hello,",first_name, last_name,"! Welcome to CTI-110" )
