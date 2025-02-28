654c76a543a13f2162d13709f84d93d2b78c859b
Kirkc9795 <kirkc9795@student.faytechcc.edu>
2025-02-28 13:32:56 -0500

    Add files via upload

---
+++ P1HW1_KirkChristopher.py
@@ -0,0 +1,13 @@
+# Chrisrtopher Kirk 
+#02/28/2025
+# This project is me calculating exponents and addition and subtraction using intergers
+base =int(input("Enter the base: "))
+exponent =int(input("Enter the exponent: ")) 
+result = base ** exponent
+print (base,"raised to the power of", exponent, "is", result)
+# Addition and aubtraction 
+num1 = int(input("Enter starting integer:"))
+num2 = int(input("Enter an intiger to add:"))
+num3 = int(input("Enter an integer to subtract:"))
+result = num1 + num2 - num3
+print(num1, "+", num2, "-", num3, "is equal to", result) 
